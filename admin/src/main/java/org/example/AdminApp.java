ba63b58658461172b78afdc943aed39dd095e65a
pmlinh21 <pmlinh21@clc.fitus.edu.vn>
2023-12-03 01:23:01 +0700

    ok

--- admin/src/main/java/org/example/AdminApp.java
+++ admin/src/main/java/org/example/AdminApp.java
@@ -1,2 +1,111 @@
-package org.example;public class AdminApp {
+package org.example;
+import javax.swing.*;
+
+public class AdminApp extends javax.swing.JFrame{
+
+    public AdminApp() {
+//         try {
+//             // set windows look and feel
+//             UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+//         } catch (Exception e) {
+//             System.out.println("Error setting Windows look and feel: " + e);
+//         }
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        navbar = new javax.swing.JPanel();
+        groupNavButton = new javax.swing.JButton();
+        userNavButton = new javax.swing.JButton();
+        dataNavButton = new javax.swing.JButton();
+        reportNavButton = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        navbar.setBackground(new java.awt.Color(23, 70, 162));
+        navbar.setPreferredSize(new java.awt.Dimension(307, 50));
+
+        groupNavButton.setBackground(new java.awt.Color(23, 70, 162));
+        groupNavButton.setForeground(new java.awt.Color(255, 255, 255));
+        groupNavButton.setIcon(new ImageIcon(getClass().getResource("/users-solid.png"))); // NOI18N
+        groupNavButton.setBorderPainted(false);
+        groupNavButton.setFocusable(false);
+        groupNavButton.setPreferredSize(new java.awt.Dimension(50, 50));
+//        groupNavButton.addActionListener(new java.awt.event.ActionListener() {
+//            public void actionPerformed(java.awt.event.ActionEvent evt) {
+//                groupNavButtonActionPerformed(evt);
+//            }
+//        });
+
+        userNavButton.setBackground(new java.awt.Color(23, 70, 162));
+        userNavButton.setForeground(new java.awt.Color(255, 255, 255));
+        userNavButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/user-solid.png"))); // NOI18N
+        userNavButton.setBorderPainted(false);
+        userNavButton.setFocusable(false);
+        userNavButton.setPreferredSize(new java.awt.Dimension(50, 50));
+//        userNavButton.addActionListener(new java.awt.event.ActionListener() {
+//            public void actionPerformed(java.awt.event.ActionEvent evt) {
+//                userNavButtonActionPerformed(evt);
+//            }
+//        });
+
+        dataNavButton.setBackground(new java.awt.Color(23, 70, 162));
+        dataNavButton.setForeground(new java.awt.Color(255, 255, 255));
+        dataNavButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/database-solid.png"))); // NOI18N
+        dataNavButton.setBorderPainted(false);
+        dataNavButton.setFocusable(false);
+        dataNavButton.setPreferredSize(new java.awt.Dimension(50, 50));
+//        dataNavButton.addActionListener(new java.awt.event.ActionListener() {
+//            public void actionPerformed(java.awt.event.ActionEvent evt) {
+//                dataNavButtonActionPerformed(evt);
+//            }
+//        });
+
+        reportNavButton.setBackground(new java.awt.Color(23, 70, 162));
+        reportNavButton.setForeground(new java.awt.Color(255, 255, 255));
+        reportNavButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/flag-solid.png"))); // NOI18N
+        reportNavButton.setBorderPainted(false);
+        reportNavButton.setFocusable(false);
+        reportNavButton.setPreferredSize(new java.awt.Dimension(50, 50));
+//        reportNavButton.addActionListener(new java.awt.event.ActionListener() {
+//            public void actionPerformed(java.awt.event.ActionEvent evt) {
+//                reportNavButtonActionPerformed(evt);
+//            }
+//        });
+
+        javax.swing.GroupLayout navbarLayout = new javax.swing.GroupLayout(navbar);
+        navbar.setLayout(navbarLayout);
+        navbarLayout.setHorizontalGroup(
+                navbarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(navbarLayout.createSequentialGroup()
+                                .addGap(55, 55, 55)
+                                .addComponent(userNavButton, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(groupNavButton, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(reportNavButton, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(dataNavButton, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        navbarLayout.setVerticalGroup(
+                navbarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(dataNavButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(reportNavButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(groupNavButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(userNavButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(navbar, java.awt.BorderLayout.PAGE_START);
+        pack();
+    }
+
+
+    private javax.swing.JPanel navbar;
+    private javax.swing.JButton userNavButton;
+    private javax.swing.JButton groupNavButton;
+    private javax.swing.JButton reportNavButton;
+    private javax.swing.JButton dataNavButton;
+
 }
--- admin/src/main/java/org/example/Main.java
+++ admin/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world admin!");
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new AdminApp().setVisible(true);
+            }
+        });
+
     }
 }
