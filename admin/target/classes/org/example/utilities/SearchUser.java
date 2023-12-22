aa0bf8c944a1a03aea4da174651a64c6afbd6229
phucnguyen1334 <nthphuc21@clc.fitus.edu.vn>
2023-12-22 17:53:56 +0700

    Resolve workspace.xml conflict

---
+++ admin/target/classes/org/example/utilities/SearchUser.java
@@ -0,0 +1,46 @@
+package org.example.utilities;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import static org.example.utilities.AdminSocket.socket;
+
+public class SearchUser extends SwingWorker<Void, Void> {
+    private String username;
+    private String fullname;
+    private String selectedString;
+
+    public SearchUser(String username, String fullname, String selectedString) {
+        this.username = username;
+        this.fullname = fullname;
+        this.selectedString = selectedString;
+    }
+    public static Object[][] request(String username, String fullname , String selectedString, Socket socket) throws IOException, ClassNotFoundException {
+
+        OutputStream outputStream = socket.getOutputStream();
+        String method = "searchUser";
+        String message = username + "\n" + fullname + "\n" + selectedString;
+        System.out.println(method);
+        outputStream.write(method.getBytes());
+        outputStream.write(message.getBytes());
+
+        // receive object
+        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+        Object[][] data = (Object[][]) objectInputStream.readObject();
+
+        return data;
+    }
+
+    @Override
+    protected Void doInBackground() throws Exception {
+        try {
+            request(username, fullname, selectedString, socket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
